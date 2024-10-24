261e7484eda17b9262c510e2a29f86cad054f63e
Tasim <tasimbillah611@gmail.com>
2024-10-24 21:06:10 +0600

    even handleing using react

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import './App.css'
+
+function App() {
+
+  function handleClick(params) {
+    alert("BUTTON - 1 CLICKED");
+  }
+
+  const handleClick2 = () => {
+    alert("BUTTON - 2 CLLICKED");
+  }
+
+  const summation = (params) => {
+    alert(params + 3);
+  }
+
+  return (
+    <>
+      <h1>react core concepts part - 2 </h1>
+      <button onClick={handleClick}>click button - 1</button> 
+      <br />
+      <button onClick={handleClick2}>click button - 2</button>
+      <br />
+      <button onClick={() => {alert("BUTTON - 3 CLICKED")}}>click button - 3</button>
+      <br />
+      <button onClick={ () => summation(5) }>summation of 5 + 3</button>
+    </>
+  )
+}
+
+export default App
